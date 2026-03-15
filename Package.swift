4732c5c6127443d21703db36e6bfa6943bbaa26b
detachhead <detachhead@users.noreply.github.com>
2026-03-15 16:32:37 +1000

    update `Package.swift` with comments about swiftlint and remove some unneeded boilerplate

--- Package.swift
+++ Package.swift
@@ -11,7 +11,11 @@ let package = Package(
         .package(url: "https://github.com/JohnSundell/CollectionConcurrencyKit.git", from: "0.1.0"),
         .package(url: "https://github.com/apple/swift-system", from: "1.6.1"),
         .package(url: "https://github.com/tsolomko/SWCompression.git", from: "4.8.0"),
-        .package(url: "https://github.com/apple/swift-log", from: "1.6.0")
+        .package(url: "https://github.com/apple/swift-log", from: "1.6.0"),
+        // cant really use swiftlint on linux atm because of these issues:
+        // https://github.com/realm/SwiftLint/issues/6541
+        // https://github.com/realm/SwiftLint/issues/6080#issuecomment-4062328742
+        // .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.63.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -22,11 +26,13 @@ let package = Package(
                 .product(name: "SystemPackage", package: "swift-system"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "OctoKit", package: "octokit.swift"),
-                .product(name: "CollectionConcurrencyKit", package: "CollectionConcurrencyKit"),
+                "CollectionConcurrencyKit",
                 "SWCompression",
-                .product(name: "Logging", package: "swift-log")
+                .product(name: "Logging", package: "swift-log"),
             ],
-            swiftSettings: [SwiftSetting.treatAllWarnings(as: .error)]
+            swiftSettings: [.treatAllWarnings(as: .error)],
+            // https://github.com/realm/SwiftLint/issues/6080#issuecomment-4062328742
+            // plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]
         )
-    ]
+    ],
 )
