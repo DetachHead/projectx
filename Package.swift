5bea714119ef412efc9eb12c6a7647d7e0ec204e
detachhead <detachhead@users.noreply.github.com>
2026-03-15 22:11:48 +1000

    fix installing swift on other platforms in ci

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "projectx",
+  platforms: [.macOS(.v10_15)],
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
     .package(url: "https://github.com/nerdishbynature/octokit.swift", from: "0.11.0"),
